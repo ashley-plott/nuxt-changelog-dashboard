7a3ad1d489e28ac91c32064ed2d034f9ec0ed314
Ashley Armstrong <ashley@plott.co.uk>
2025-09-17 14:40:58 +0100

    remove .vercel

--- .vercel/output/functions/__fallback.func/chunks/build/error-404-styles.DsMtg5yZ.mjs
+++
@@ -1,6 +0,0 @@
-import { e as error404_vue_vue_type_style_index_0_scoped_6ce9f070_lang } from './error-404-styles-1.mjs-DGlTytP0.mjs';
-
-const error404Styles_DsMtg5yZ = [error404_vue_vue_type_style_index_0_scoped_6ce9f070_lang, error404_vue_vue_type_style_index_0_scoped_6ce9f070_lang];
-
-export { error404Styles_DsMtg5yZ as default };
-//# sourceMappingURL=error-404-styles.DsMtg5yZ.mjs.map
